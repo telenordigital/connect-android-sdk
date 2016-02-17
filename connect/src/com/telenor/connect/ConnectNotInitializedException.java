e32c48c5e75e74d589dc983a2313d2867e5cb7de
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2016-02-17 13:31:30 +0100

    Remove redundant import line
    Change-Id: I5b6a01b0f8efae1531c30665e470465a9f1ee207

--- connect/src/com/telenor/connect/ConnectNotInitializedException.java
+++ connect/src/com/telenor/connect/ConnectNotInitializedException.java
@@ -1,7 +1,5 @@
 package com.telenor.connect;
 
-import com.telenor.connect.ConnectException;
-
 public class ConnectNotInitializedException extends ConnectException {
     public ConnectNotInitializedException(String message) {
         super(message);
