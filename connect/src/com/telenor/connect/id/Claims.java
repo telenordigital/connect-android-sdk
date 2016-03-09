cc6048c802c288a4dfe6ab6dfd2d9cc81a243d7f
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2016-03-09 12:35:43 +0100

    Add openid and address to Claims class
    Change-Id: I70d6774e9d70de1c373431bf3b7b21683e520321

--- connect/src/com/telenor/connect/id/Claims.java
+++ connect/src/com/telenor/connect/id/Claims.java
@@ -6,8 +6,10 @@ import java.util.Set;
 
 public class Claims {
 
+    public static final String OPENID = "openid";
     public static final String PROFILE = "profile";
     public static final String EMAIL = "email";
+    public static final String ADDRESS = "address";
     public static final String PHONE = "phone";
 
     private final Set<String> claims;
