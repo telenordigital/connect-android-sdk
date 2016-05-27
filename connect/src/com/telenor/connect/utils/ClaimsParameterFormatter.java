46bb8ea9d6923d6ba8cbd0988080991f001eaff0
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2016-05-27 14:44:23 +0200

    Give access to ID token fields (#28)
    * Allow access to ID token fields
    * Update IdToken toString with new fields
    * Change order of statements in IdToken
    * Extract constants in IdToken
    * Clearify readme on getting user info
    * Correct Claim values

--- connect/src/com/telenor/connect/utils/ClaimsParameterFormatter.java
+++ connect/src/com/telenor/connect/utils/ClaimsParameterFormatter.java
@@ -9,6 +9,21 @@ public class ClaimsParameterFormatter {
 
     private static final String USER_INFO = "userinfo";
 
+    /**
+     * Following the OpenID Connect 1.0 spec this function encodes a set of Claims as essential,
+     * for use as a request parameter.
+     * Essential Claim definition: "Claim specified by the Client as being necessary to ensure a
+     * smooth authorization experience for the specific task requested by the End-User."
+     *
+     * In other words, after successfully authorizing the request with the given Claims, the user
+     * will have data in the given fields.
+     *
+     * See http://openid.net/specs/openid-connect-core-1_0.html#ClaimsParameter for more details.
+     *
+     * @param claims A claims object containing wanted claims.
+     * @return A JSON formatted string with all {@code claims} turned into essential claims.
+     * @throws JSONException
+     */
     public static String asJson(Claims claims) throws JSONException {
         final JSONObject essentialTrue = new JSONObject().put("essential", true);
 
