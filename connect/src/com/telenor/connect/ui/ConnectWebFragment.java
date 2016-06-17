ced143d5d8cab5df1a64722bb0615ef8216ca952
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2016-06-17 15:56:03 +0200

    Improve network error screen (#35)
    * Improve network error screen
    * Change versionName to v0.5.1

--- connect/src/com/telenor/connect/ui/ConnectWebFragment.java
+++ connect/src/com/telenor/connect/ui/ConnectWebFragment.java
@@ -1,6 +1,8 @@
 package com.telenor.connect.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewStub;
 import android.webkit.WebView;
+import android.widget.Button;
 
 import com.telenor.connect.R;
 import com.telenor.connect.utils.ConnectUrlHelper;
@@ -29,21 +32,63 @@ public class ConnectWebFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         final View view
                 = inflater.inflate(R.layout.com_telenor_connect_web_fragment, container, false);
-        View errorView = view.findViewById(R.id.com_telenor_connect_error_view);
-        ViewStub loadingView = (ViewStub) view.findViewById(R.id.com_telenor_connect_loading_view);
+        final WebView webView
+                = (WebView) view.findViewById(R.id.com_telenor_connect_fragment_webview);
+        final ViewStub loadingView
+                = (ViewStub) view.findViewById(R.id.com_telenor_connect_loading_view);
         final int loadingScreenResource = getArguments()
                 .getInt(ConnectUtils.CUSTOM_LOADING_SCREEN_EXTRA,
                         R.layout.com_telenor_connect_default_loading_view);
         loadingView.setLayoutResource(loadingScreenResource);
         loadingView.inflate();
         loadingView.setVisibility(View.VISIBLE);
+        final String pageUrl = ConnectUrlHelper.getPageUrl(getArguments(), getActivity());
+        final View errorView = view.findViewById(R.id.com_telenor_connect_error_view);
+        setupErrorView(webView, loadingView, pageUrl, errorView, view);
 
-        WebView webView = (WebView) view.findViewById(R.id.com_telenor_connect_fragment_webview);
         client = new ConnectWebViewClient(getActivity(), webView, loadingView, errorView);
 
-        String pageUrl = ConnectUrlHelper.getPageUrl(getArguments(), getActivity());
         WebViewHelper.setupWebView(webView, client, pageUrl);
         return view;
+    }
+
+    private void setupErrorView(
+            final WebView webView,
+            final ViewStub loadingView,
+            final String pageUrl,
+            final View errorView,
+            final View view) {
+        final View loadingSpinner
+                = errorView.findViewById(R.id.com_telenor_connect_error_view_loading);
+        final Button tryAgain
+                = (Button) errorView.findViewById(R.id.com_telenor_connect_error_view_try_again);
+        tryAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingSpinner.setVisibility(View.VISIBLE);
+                tryAgain.setEnabled(false);
+
+                view.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        loadingSpinner.setVisibility(View.INVISIBLE);
+                        tryAgain.setEnabled(true);
+                        errorView.setVisibility(View.GONE);
+                        loadingView.setVisibility(View.VISIBLE);
+                        webView.loadUrl(pageUrl);
+                    }
+                }, 1000);
+            }
+        });
+        final Button networkSettings = (Button) errorView
+                .findViewById(R.id.com_telenor_connect_error_view_network_settings);
+        networkSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
