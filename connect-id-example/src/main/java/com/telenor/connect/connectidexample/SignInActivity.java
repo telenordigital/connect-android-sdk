ad41dd3ba41993be4b35a17b8a5ad83f9932cda0
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2016-07-04 14:49:47 +0200

    Use Chrome Tabs for id example
    Change-Id: If63f7bf86a343ec0e7f75b2be6fe4a5237a5bc5a

--- connect-id-example/src/main/java/com/telenor/connect/connectidexample/SignInActivity.java
+++ connect-id-example/src/main/java/com/telenor/connect/connectidexample/SignInActivity.java
@@ -3,18 +3,66 @@ package com.telenor.connect.connectidexample;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-import com.telenor.connect.ui.ConnectLoginButton;
+import com.telenor.connect.ConnectCallback;
+import com.telenor.connect.ConnectSdk;
+import com.telenor.connect.ui.ConnectLoginChromeTabButton;
+import com.telenor.connect.utils.ConnectUtils;
 
 public class SignInActivity extends Activity {
+
+    private View progressBar;
+    private ConnectLoginChromeTabButton loginButton;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ConnectSdk.intentHasValidRedirectUrlCall(getIntent())) {
+            progressBar.setVisibility(View.VISIBLE);
+            loginButton.setEnabled(false);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            loginButton.setEnabled(true);
+            loginButton.preLoad();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        loginButton = (ConnectLoginChromeTabButton) findViewById(R.id.login_button);
+        progressBar = findViewById(R.id.progress_bar);
 
-        ConnectLoginButton loginButton = (ConnectLoginButton) findViewById(R.id.login_button);
         loginButton.setLoginScopeTokens("profile openid");
+        final View.OnClickListener buttonClickListener = loginButton.getOnClickListener();
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                loginButton.setEnabled(false);
+                buttonClickListener.onClick(v);
+            }
+        });
+
+        ConnectSdk.checkIntentForAndHandleRedirectUrlCall(getIntent(), new ConnectCallback() {
+            @Override
+            public void onSuccess(Object successData) {
+                final Intent startIntent
+                        = new Intent(getApplicationContext(), SignedInActivity.class);
+                startIntent.setFlags(
+                        Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(startIntent);
+                finish();
+            }
+
+            @Override
+            public void onError(Object errorData) {
+                Log.e(ConnectUtils.LOG_TAG, errorData.toString());
+            }
+        });
     }
 
     @Override
@@ -26,5 +74,4 @@ public class SignInActivity extends Activity {
             finish();
         }
     }
-
 }
