e8266eb0fb12afc2fabdfb4c4ce432f4dfd25ad6
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2016-09-14 16:19:52 +0200

    Clean up extractDate in HeadersDateUtil
    Change-Id: I8165a3efa60a922d65e78d834cde087de7859de2

--- connect/src/com/telenor/connect/utils/HeadersDateUtil.java
+++ connect/src/com/telenor/connect/utils/HeadersDateUtil.java
@@ -3,6 +3,7 @@ package com.telenor.connect.utils;
 import android.annotation.SuppressLint;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -19,14 +20,16 @@ public class HeadersDateUtil {
 
     @Nullable
     public static Date extractDate(@NonNull List<Header> headers) {
-        try {
-            for (Header header : headers) {
-                if (header.getName().equalsIgnoreCase("Date")) {
+        for (Header header : headers) {
+            if (header.getName().equalsIgnoreCase("Date")) {
+                try {
                     return DATE_FORMAT.parse(header.getValue());
+                } catch (ParseException e) {
+                    Log.e(ConnectUtils.LOG_TAG, "Failed to call parse Date from headers. " +
+                            "headers=" + headers, e);
+                    return null;
                 }
             }
-        } catch (ParseException e) {
-            e.printStackTrace();
         }
         return null;
     }
