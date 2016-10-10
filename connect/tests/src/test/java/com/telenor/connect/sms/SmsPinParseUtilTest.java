05c8b0d1361750b6da0397645384d78c10b61056
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2016-10-10 12:08:28 +0200

    Add Burmese pin parse tests
    Change-Id: Ib725e2ea7cfa39065f2793b9bcdd3c909f537dd0

--- connect/tests/src/test/java/com/telenor/connect/sms/SmsPinParseUtilTest.java
+++ connect/tests/src/test/java/com/telenor/connect/sms/SmsPinParseUtilTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static junit.framework.Assert.assertNull;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
@@ -108,7 +107,7 @@ public class SmsPinParseUtilTest {
         String body = "Telenor are currently having connectivity issues. Expected fix time" +
                 " is by 20:00 tonight.";
         String actual = SmsPinParseUtil.findPin(body, instruction);
-        assertNull(actual);
+        assertThat(actual, nullValue());
     }
 
     @Test
@@ -117,7 +116,7 @@ public class SmsPinParseUtilTest {
         String body = "\u0e23\u0e2b\u0e31\u0e2a\u0e22\u0e37\u0e19\u0e22\u0e31\u0e19\u0e02\u0e2d\u0e07\u0e04\u0e38\u0e13\u0e2a\u0e33\u0e2b\u0e23\u0e31\u0e1a \u0e04\u0e37\u0e2d  - CONNECT \u0e42\u0e14\u0e22 Telenor Digital";
         // same as: "รหัสยืนยันของคุณสำหรับ คือ  - CONNECT โดย Telenor Digital"
         String actual = SmsPinParseUtil.findPin(body, instruction);
-        assertNull(actual);
+        assertThat(actual, nullValue());
     }
 
     @Test
@@ -134,6 +133,22 @@ public class SmsPinParseUtilTest {
         final Instruction instruction = get4DigitPinInstruction();
         String body = "\u0627\u0653\u067e \u0646\u06d2 \u063a\u0644\u0637 \u067e\u0627\u0633 \u0648\u0631\u0688 \u062f\u0631\u062c \u06a9\u06cc\u0627 \u06c1\u06d2\u06d4 \u0628\u0631\u0627\u06c1\u0650 \u0645\u06c1\u0631\u0628\u0627\u0646\u06cc \u062f\u0648\u0628\u0627\u0631\u06c1 \u06a9\u0648\u0634\u0634 \u06a9\u0631\u06cc\u06ba \u06cc\u0627 <a href=\"{0}\">\u0627\u067e\u0646\u0627 \u067e\u0627\u0633 \u0648\u0631\u0688 \u062f\u0648\u0628\u0627\u0631\u06c1 \u062a\u0631\u062a\u06cc\u0628 \u062f\u06cc\u06ba</a>\u06d4";
         String actual = SmsPinParseUtil.findPin(body, instruction);
-        assertNull(actual);
+        assertThat(actual, nullValue());
+    }
+
+    @Test
+    public void findsPinInBurmeseSms() {
+        final Instruction instruction = get4DigitPinInstruction();
+        String body = "'Hipstagram' ဟာ သင့်ဖုန်းနံပါတ် မှန်ကန်ကြောင်း သက်သေပြမယ့် ကုတ်နံပါတ်က 3456 ဖြစ်ပါတယ်။ Telenor Digital ရဲ့ CONNECT";
+        String actual = SmsPinParseUtil.findPin(body, instruction);
+        assertThat(actual, is("3456"));
+    }
+
+    @Test
+    public void lookingForMissingPinInBurmeseReturnsNull() {
+        final Instruction instruction = get4DigitPinInstruction();
+        String body = "<s>99999999</s> သို့ မှန်ကန်ကြောင်းအတည်ပြုတဲ့ ကုတ်နံပါတ် ပို့ပေးလိုက်ပါပြီ။ ကျေးဇူးပြုပြီး သင့်ဖုန်းနံပါတ်ကို အတည်ပြုဖို့ အဲဒီကုတ်နံပါတ်ကို အောက်ဖက်မှာ ရိုက်ထည့်လိုက်ပါ။";
+        String actual = SmsPinParseUtil.findPin(body, instruction);
+        assertThat(actual, nullValue());
     }
 }
