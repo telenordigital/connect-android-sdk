aaac2427096cedc9ff2a391c648e3fca72cf967c
gorandostanic <goran@telenordigital.com>
2017-05-01 19:18:29 +0200

    Reformatting OperatorDiscoveryConfig.java

--- connect/src/com/telenor/mobileconnect/operatordiscovery/OperatorDiscoveryConfig.java
+++ connect/src/com/telenor/mobileconnect/operatordiscovery/OperatorDiscoveryConfig.java
@@ -2,10 +2,7 @@ package com.telenor.mobileconnect.operatordiscovery;
 
 public interface OperatorDiscoveryConfig {
     String getOperatorDiscoveryClientId();
-
     String getOperatorDiscoveryClientSecret();
-
     String getOperatorDiscoveryRedirectUri();
-
     String getOperatorDiscoveryEndpoint();
 }
