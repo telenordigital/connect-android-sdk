dec632291e28ca6bd3287685abf1bca8992cf10f
gorandostanic <goran@telenordigital.com>
2017-05-02 14:39:27 +0200

    Remormatting SdkProfile

--- connect/src/com/telenor/connect/SdkProfile.java
+++ connect/src/com/telenor/connect/SdkProfile.java
@@ -7,25 +7,16 @@ import java.util.List;
 import java.util.Map;
 
 public interface SdkProfile {
+
     HttpUrl getApiUrl();
-
     String getClientId();
-
     String getClientSecret();
-
     boolean isConfidentialClient();
-
     String getRedirectUri();
-
     ConnectIdService getConnectIdService();
-
     String getExpectedIssuer(String actualIssuer);
-
     List<String> getExpectedAudiences(List<String> actualAudiences);
 
-    enum DoNext {proceed, cancel}
-
-    ;
-
+    enum DoNext {proceed, cancel};
     DoNext onAuthorize(Map<String, String> parameters);
 }
