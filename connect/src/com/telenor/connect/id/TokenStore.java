d5c22f27513cc2d4bb9cafe3a892b524b334ddb5
Atanas Bakalov <atanas.bakalov@telenordigital.com>
2017-08-16 11:16:36 +0200

    Force the date format of the jsons in token store
    The different cases handled in DateDeserializer::deserialize are for
    backward compatibility with existing tokens. In the future they should
    be removed, and only the base case:
    SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(date)
    should be left.

--- connect/src/com/telenor/connect/id/TokenStore.java
+++ connect/src/com/telenor/connect/id/TokenStore.java
@@ -3,14 +3,62 @@ package com.telenor.connect.id;
 import android.content.Context;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSyntaxException;
+
+import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class TokenStore {
 
-    public static final String PREFERENCES_FILE = "com.telenor.connect.PREFERENCES_FILE";
-
     private static final String PREFERENCE_KEY_CONNECT_TOKENS = "CONNECT_TOKENS";
     private static final String PREFERENCE_KEY_ID_TOKEN = "ID_TOKEN";
-    private static final Gson preferencesGson = new Gson();
+    public static final String PREFERENCES_FILE = "com.telenor.connect.PREFERENCES_FILE";
+    private static final Gson preferencesGson =
+            new GsonBuilder()
+                    .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                    .registerTypeAdapter(Date.class, new DateDeserializer())
+                    .create();
+
+    private static class DateDeserializer implements JsonDeserializer<Date> {
+        @Override
+        public Date deserialize(JsonElement je, Type type, JsonDeserializationContext jdc)
+                throws JsonParseException {
+            String date = je.getAsString();
+
+            try {
+                return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(date);
+            } catch (ParseException e) {}
+
+            try {
+                return new SimpleDateFormat("MMM d, y h:mm:ss").parse(date);
+            } catch (ParseException e) {}
+
+            try {
+                return new SimpleDateFormat("MMM d, y h:mm:ss", Locale.US).parse(date);
+            } catch (ParseException e) {}
+
+            try {
+                return DateFormat
+                        .getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT).parse(date);
+            } catch (ParseException e) {}
+
+            try {
+                return DateFormat.getDateTimeInstance(
+                        DateFormat.DEFAULT, DateFormat.DEFAULT, Locale.US).parse(date);
+            } catch (ParseException e) {}
+
+            throw new JsonParseException("Invalid date:" + date);
+        }
+    }
 
     private final Context context;
 
@@ -42,8 +90,12 @@ public class TokenStore {
         String connectTokensJson = context
                 .getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE)
                 .getString(PREFERENCE_KEY_CONNECT_TOKENS, null);
-
-        return preferencesGson.fromJson(connectTokensJson, ConnectTokens.class);
+        try {
+            return preferencesGson.fromJson(connectTokensJson, ConnectTokens.class);
+        } catch (JsonSyntaxException e) {
+            clear();
+            return null;
+        }
     }
 
     public void clear() {
@@ -58,6 +110,11 @@ public class TokenStore {
         String idTokenJson = context
                 .getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE)
                 .getString(PREFERENCE_KEY_ID_TOKEN, null);
-        return preferencesGson.fromJson(idTokenJson, IdToken.class);
+        try {
+            return preferencesGson.fromJson(idTokenJson, IdToken.class);
+        } catch (JsonSyntaxException e) {
+            clear();
+            return null;
+        }
     }
 }
