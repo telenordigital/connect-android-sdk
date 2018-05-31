debc3d7b61ee9f9bd4343b8c964e45a526e25bd2
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2018-05-31 14:45:01 +0200

    Add getValidAccessToken and getUserInfo example
    Change-Id: Ic4ada54a3c1968b86ebc92aabd7252e57f34633b

--- connect-id-example/src/main/java/com/telenor/connect/connectidexample/SignedInActivity.java
+++ connect-id-example/src/main/java/com/telenor/connect/connectidexample/SignedInActivity.java
@@ -6,9 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.telenor.connect.ConnectSdk;
+import com.telenor.connect.id.AccessTokenCallback;
 import com.telenor.connect.id.ConnectTokensStateTracker;
+import com.telenor.connect.id.UserInfo;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class SignedInActivity extends Activity {
 
@@ -32,9 +39,6 @@ public class SignedInActivity extends Activity {
         TextView userId = findViewById(R.id.user_id);
         userId.setText(ConnectSdk.getIdToken().getSubject());
 
-        if (ConnectSdk.getAccessToken() == null) {
-            goToLogin();
-        }
         new ConnectTokensStateTracker() {
             @Override
             protected void onTokenStateChanged(boolean hasTokens) {
@@ -43,6 +47,29 @@ public class SignedInActivity extends Activity {
                 }
             }
         };
+
+        ConnectSdk.getValidAccessToken(new AccessTokenCallback() {
+            @Override
+            public void onSuccess(String accessToken) {
+                Toast.makeText(SignedInActivity.this, "Got valid access token: " + accessToken, Toast.LENGTH_LONG).show();
+                ConnectSdk.getUserInfo(new Callback<UserInfo>() {
+                    @Override
+                    public void success(UserInfo userInfo, Response response) {
+                        Toast.makeText(SignedInActivity.this, "Got userInfo: " + userInfo.toString(), Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        Toast.makeText(SignedInActivity.this, "Got getUserInfo error: " + error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onError (Object errorData){
+                Toast.makeText(SignedInActivity.this, "Got getValidAccessToken error: " + errorData.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void goToLogin() {
