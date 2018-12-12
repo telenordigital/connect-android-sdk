7f5b369235a05ffed0e607c8ab17ab908cf76307
Arno Schoonbee <soldierinwhite@users.noreply.github.com>
2018-12-12 12:16:18 +0100

    Workaround for chromeTabs for apps using Jetifier (#140)
    * Thwart Jetifier with a horrible hack

--- connect/src/com/telenor/connect/utils/CustomTabsHelper.java
+++ connect/src/com/telenor/connect/utils/CustomTabsHelper.java
@@ -39,12 +39,12 @@ public class CustomTabsHelper {
     static final String BETA_PACKAGE = "com.chrome.beta";
     static final String DEV_PACKAGE = "com.chrome.dev";
     static final String LOCAL_PACKAGE = "com.google.android.apps.chrome";
-    private static final String EXTRA_CUSTOM_TABS_KEEP_ALIVE =
-            "android.support.customtabs.extra.KEEP_ALIVE";
-    private static final String ACTION_CUSTOM_TABS_CONNECTION =
-            "android.support.customtabs.action.CustomTabsService";
 
     private static String sPackageNameToUse;
+    
+    @SuppressWarnings("StringBufferReplaceableByString")
+    private static final String ACTION_CUSTOM_TABS_CONNECTION = new StringBuilder("android")
+            .append(".support.customtabs.action.CustomTabsService").toString();
 
     private CustomTabsHelper() {}
 
