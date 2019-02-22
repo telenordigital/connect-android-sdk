a953ea609f2caab5f16977632ea09679f0e9cfec
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2019-02-22 15:09:21 +0100

    Clean up connect custom tab login button (#153)
    * Remove WeakReferences and clean up ConnectCustomTabLoginButton
    * Now sets/clears session on attach/detached from window events.
    * Button is disabled when clicked
      * Enabled onVisibilityChanged, unless there is a valid redirect url
      (in which case it stays disabled so authorization code can be
      exchanged for tokens, without the user starting a new session)

--- connect/src/com/telenor/connect/ui/ConnectCustomTabLoginButton.java
+++ connect/src/com/telenor/connect/ui/ConnectCustomTabLoginButton.java
@@ -5,9 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.customtabs.CustomTabsCallback;
 import android.support.customtabs.CustomTabsClient;
 import android.support.customtabs.CustomTabsServiceConnection;
 import android.support.customtabs.CustomTabsSession;
@@ -20,27 +18,27 @@ import com.telenor.connect.ConnectSdk;
 import com.telenor.connect.utils.ConnectUrlHelper;
 import com.telenor.connect.utils.CustomTabsHelper;
 
-import java.lang.ref.WeakReference;
-
 /**
  * Uses custom tabs, unless not available. If not available falls back to ConnectWebViewLoginButton
  * logic.
  */
 public class ConnectCustomTabLoginButton extends ConnectWebViewLoginButton {
 
-    private static final Uri PRE_FETCH_URL
-            = Uri.parse(
-            ConnectUrlHelper
-                    .getConnectApiUrl()
-                    .newBuilder()
-                    .addPathSegment("id")
-                    .addPathSegment("android-sdk-prefetch-static-resources")
-                    .build()
-                    .uri()
-                    .toString()
-    );
+    final private CustomTabsServiceConnection connection = new CustomTabsServiceConnection() {
+        @Override
+        public void onCustomTabsServiceConnected(ComponentName componentName, CustomTabsClient client) {
+            client.warmup(0);
+            final CustomTabsSession session = client.newSession(null);
+            ConnectCustomTabLoginButton.this.session = session;
+            session.mayLaunchUrl(ConnectUrlHelper.getPreFetchUrl(), null, null);
+        }
 
-    private CustomTabsServiceConnection connection;
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            session = null;
+        }
+    };
+
     private boolean customTabsSupported = false;
     private boolean serviceBound = false;
     private BrowserType browserType;
@@ -52,6 +50,7 @@ public class ConnectCustomTabLoginButton extends ConnectWebViewLoginButton {
 
     @Override
     protected void authenticate() {
+        setEnabled(false);
         if (!customTabsSupported) {
             super.authenticate();
             return;
@@ -62,6 +61,19 @@ public class ConnectCustomTabLoginButton extends ConnectWebViewLoginButton {
                 browserType,
                 getActivity(),
                 getShowLoadingCallback());
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        String packageNameToUse = CustomTabsHelper.getPackageNameToUse(getContext());
+        if (TextUtils.isEmpty(packageNameToUse)) {
+            return;
+        }
+        serviceBound = CustomTabsClient.bindCustomTabsService(getContext(), packageNameToUse, connection);
+        boolean correctIntentFilter = contextIntentFilterMatchesRedirectUri(getContext());
+        customTabsSupported = serviceBound && correctIntentFilter;
+        browserType = customTabsSupported ? BrowserType.CHROME_CUSTOM_TAB : BrowserType.WEB_VIEW;
     }
 
     private static boolean contextIntentFilterMatchesRedirectUri(Context context) {
@@ -76,29 +88,13 @@ public class ConnectCustomTabLoginButton extends ConnectWebViewLoginButton {
     }
 
     @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        String packageNameToUse = CustomTabsHelper.getPackageNameToUse(getContext());
-        if (TextUtils.isEmpty(packageNameToUse)) {
-            return;
-        }
-        connection = new WeakReferenceCustomTabsServiceConnection(new WeakReference<>(this));
-        serviceBound = CustomTabsClient.bindCustomTabsService(getContext(), packageNameToUse, connection);
-        boolean correctIntentFilter = contextIntentFilterMatchesRedirectUri(getContext());
-        customTabsSupported = serviceBound && correctIntentFilter;
-        browserType = customTabsSupported ? BrowserType.CHROME_CUSTOM_TAB : BrowserType.WEB_VIEW;
-    }
-
-    @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         if (serviceBound) {
             getContext().unbindService(connection);
             serviceBound = false;
         }
-        if (connection != null) {
-            connection = null;
-        }
+        session = null;
     }
 
     @Override
@@ -108,62 +104,9 @@ public class ConnectCustomTabLoginButton extends ConnectWebViewLoginButton {
         if (ConnectSdk.hasErrorRedirectUrlCall(intent)) {
             ConnectSdk.setRandomLogSessionId();
         }
-    }
 
-    private void setSession(CustomTabsSession session) {
-        this.session = session;
-    }
-
-    private static class WeakReferenceCustomTabsServiceConnection extends CustomTabsServiceConnection {
-
-        private final WeakReference<ConnectCustomTabLoginButton> weakButton;
-
-        WeakReferenceCustomTabsServiceConnection(WeakReference<ConnectCustomTabLoginButton> weakButton) {
-            this.weakButton = weakButton;
-        }
-
-        @Override
-        public void onCustomTabsServiceConnected(ComponentName name, CustomTabsClient client) {
-            final ConnectCustomTabLoginButton connectCustomTabLoginButton = weakButton.get();
-            if (connectCustomTabLoginButton == null) {
-                return;
-            }
-            client.warmup(0);
-            final CustomTabsSession session = client.newSession(new WeakReferenceCustomTabsCallback(weakButton));
-            connectCustomTabLoginButton.setSession(session);
-            if (session != null) {
-                session.mayLaunchUrl(PRE_FETCH_URL, null, null);
-            }
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-        }
-    }
-
-    private static class WeakReferenceCustomTabsCallback extends CustomTabsCallback {
-
-        private final WeakReference<ConnectCustomTabLoginButton> weakButton;
-
-        WeakReferenceCustomTabsCallback(WeakReference<ConnectCustomTabLoginButton> weakButton) {
-            this.weakButton = weakButton;
-        }
-
-        @Override
-        public void onNavigationEvent(int navigationEvent, Bundle extras) {
-            final ConnectCustomTabLoginButton connectCustomTabLoginButton = weakButton.get();
-            if (connectCustomTabLoginButton == null) {
-                return;
-            }
-            switch (navigationEvent) {
-                case CustomTabsCallback.TAB_HIDDEN:
-                    connectCustomTabLoginButton.setEnabled(true);
-                    return;
-                case CustomTabsCallback.TAB_SHOWN:
-                    connectCustomTabLoginButton.setEnabled(false);
-                    return;
-                default:
-            }
-        }
+        boolean ongoingAuth = intent != null && ConnectSdk.hasValidRedirectUrlCall(intent);
+        boolean enableButton = !ongoingAuth;
+        setEnabled(enableButton);
     }
 }
